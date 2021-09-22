10b7c6179ae15550f1c3a81fd11920f781c5ed9d
Adbhuta Gauranga das <adbhutagaurangadas.gkg@gmail.com>
2021-09-22 11:58:08 +0300

    add custom operation meta support (#19)
    * add custom operation meta support to `HasHistories` trait

--- src/HasHistories.php
+++ src/HasHistories.php
@@ -39,6 +39,43 @@ trait HasHistories
     }
 
     /**
+     * Get the model's meta in history.
+     *
+     * @return array
+     */
+    public function getModelMeta($event)
+    {
+        switch($event)
+        {
+            case 'updating':
+                /*
+                * Gets the model's altered values and tracks what had changed
+                */
+                $changes = $this->getDirty();
+
+                $changed = [];
+                foreach ($changes as $key => $value) {
+                    if(static::isIgnored($this, $key)) continue;
+
+                    array_push($changed, ['key' => $key, 'old' => $this->getOriginal($key), 'new' => $this->$key]);
+                }
+                return $changed;
+            case 'created':
+            case 'deleting':
+            case 'restored':
+                return null;
+        }
+    }
+
+    public static function isIgnored($model, $key)
+    {
+        $blacklist = config('history.attributes_blacklist');
+        $name = get_class($model);
+        $array = isset($blacklist[$name])? $blacklist[$name]: null;
+        return !empty($array) && in_array($key, $array);
+    }
+
+    /**
      * Get the model's label in history.
      *
      * @return string
